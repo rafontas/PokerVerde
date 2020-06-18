f888a7f1eddb319217ab66230a7d5c8857e9b1ca
Rafael Freitas <rafontas@gmail.com>
2020-06-18 12:13:47 -0300

    Iniciado teste para as estatísticas do jogador

---
+++ Comum/Classes/RodadaTHB.cs
@@ -0,0 +1,17 @@
+﻿using Comum.AbstractClasses;
+using Enuns;
+using Modelo;
+
+namespace Comum.Classes
+{
+    public class RodadaTHB : RodadaBase
+    {
+        public uint ValorApostadoRodadaJogador { get; set; }
+        public uint ValorApostadoRodadaBanca { get; set; }
+
+        // Ver como fazer um controle melhor de rodada. Se adiciono Lista de acao e a cada acao, da um push? Parece uma boa ideia
+        // Lembrar de terminar de corrigir o bug do retorno dos valores no empate
+
+        public RodadaTHB(TipoRodada tp, uint valorPotePreRodada, Carta[] cartaMesa) : base(cartaMesa, valorPotePreRodada, tp) { }
+    }
+}
---
+++ Comum/Interfaces/IJogadorEstatisticas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comum.Interfaces
+{
+    public interface IJogadorEstatisticas
+    {
+        int getQuantidadeJogosJogados();
+        int getStackInicial();
+        int getStackFinal();
+        int getStackSaldoFinal();
+        int getQuantidadeJogosGanhos();
+        int getQuantidadeJogosPerdidos();
+        float getValorGanhoPorJogo();
+    }
+}
